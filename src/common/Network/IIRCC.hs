ace3d6a4bd96f8e2548c451cbc5a4ef83c306b87
Matthew Stewart <m.scott.stewart@gmail.com>
2017-04-23 02:36:54 +0000

    Initial commit.  Hello world!

---
+++ src/common/Network/IIRCC.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DefaultSignatures #-}
+
+module Network.IIRCC (
+) where
---
+++ src/ircui/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Network.IIRCC
+
+main :: IO ()
+main = putStrLn "Hello World!"
